53c4526b993ea88c38d6fa6829e3dd2a920f9bee
Saul Urias <saul_ulises55@hotmail.com>
2016-10-04 14:37:03 -0700

    Se modifico el metodo while, agregando un Do-While

--- src/reglafalsa/ReglaFalsa.java
+++ src/reglafalsa/ReglaFalsa.java
@@ -15,35 +15,64 @@ public class ReglaFalsa {
      * @param args the command line arguments
      */
    public static double f (double x){
-       //Insertar la función 
+       
         double e = 2.718281828182;
-        
-        double resultado = Math.pow(e, -1*x) -x;
+    
+        //Definimos una funcion de variable real
+        double resultado = Math.pow(e, -1*x) -x; //Valor de la función
         
         return resultado;
     }
-    
+   
+   public static double errorRelativo(double a, double b){
+        double resultado = Math.abs( ( (a-b) / a )*100 );
+        return resultado;
+    }
+    //Probamos en el programa principal
     public static void main(String[] args) {
         
-        double a = 0, b = 1, c = 0;
+        double a = 0, b = 1, c = 0, ea = 0.01;
+        
+        double error = 0, errorAnterior =0;
+        double calculoError = 0;
+        int iteraciones = 0;
+        
+        
+        
         
         if (f(a)*f(b)>0){
             System.out.println("No se cumple que f(a)*f(b)<0.");
             return;
         }
-        while (true){
+        
+        do {
+            errorAnterior = error; //guardar error anterior
             
-            c = ((f(a)*b)-(f(b)*a))/(f(a)-f(b));
-            if (Math.abs(f(c))<=1e-10){
-                System.out.println("La raiz estimada es r = " + c);
-                System.out.println("El valor de f en rs = " + f(c));
-                return;
-            }
-            if (f(a)*f(c)<0) {
+            c = ((f(a)*b)-(f(b)*a))/(f(a)-f(b)); //calcular c
+            error = c; //guardar error actual
+            
+            //System.out.println(error+" - "+ errorAnterior+" / "+error);
+            calculoError = errorRelativo(error, errorAnterior); //calcular el error relativo
+            //System.out.println("abs "+calculoError);
+            
+            //condicional para reacomodar los limites que estan calculando la raiz
+            if(f(a)*f(c) < 0) { 
                 b = c;
-            }else{
+            }
+            else{
                 a = c;
             }
-        }
+            
+            
+            iteraciones++;
+        } while (a != b && calculoError > ea);
+         
+         System.out.println("La raiz estimada es c = " + c);
+         System.out.println("El valor de f en c = " + f(c));
+        
+        
+        
+      
     }
+    
 }
